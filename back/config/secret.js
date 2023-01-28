dc2fc2e0595cbbc713859b56755cc5c484118742
bathwan <ehfdkdhswlgh@naver.com>
2023-01-28 19:23:07 +0900

    수정

--- back/config/secret.js
+++ back/config/secret.js
@@ -1,11 +1,11 @@
 module.exports = {
     jwtsecret: "jwt_secret_key_9312",
-    host: "127.0.0.1",
+    host: "127.0.0.1", 
     user: "root",
     port: "3306",
-    password: "rmadhrhd1!",
-    database: "blog",
-    intervalTime: 60*60*1000, //1시간
+    password: "rmadhrhd1!", 
+    database: "blog", 
+    intervalTime: 60*60*1000, // demonFunction에 정의된 함수를 주기적으로 실행시킬 시간 설정 (1시간)
     publicDataKey: '=yy48ZhZI0vb1gOljRi03%2BaL38Qu8P6nnA%2FhrUbXdAKLrt9u9pa2Lk4yfkE9PGQC%2Bj12DTPVh13EItiaamkdJ1w%3D%3D',// 맨 앞에 '=' 붙혀주세요
     dateStrings : 'date',
 
